5d7ae9f671d57a5282e511fbe3cc423b82cf5039
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2021-02-28 14:09:40 +0500

    Добавлена функция перелистывания слайдера

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+
+
+var num_slide = 0; 
+
 jQuery(document).ready(function($) {
 	$('.info-house_points_item').on('click', function() {
 		if ($(this).hasClass("active")) {
@@ -5,6 +9,26 @@ jQuery(document).ready(function($) {
 		}else{
 			$('.info-house_points .info-house_points_item').removeClass('active');
 			$(this).addClass('active');
+		}
+	});
+
+	$('.gallery-block_head_slider_next').on('click', function() {
+		if (num_slide < 2) {
+			num_slide += 1;
+			mg_line = (num_slide * 100) * -1;
+			$('.gallery-block_slider_line').css('transform', 'translateX('+mg_line+'%)');
+			
+			$('.gallery-block_head_slider_number_item.number_page').text(num_slide + 1);
+		}
+	});
+
+	$('.gallery-block_head_slider_prev').on('click', function() {
+		if (num_slide > 0) {
+			num_slide -= 1;
+			mg_line = (num_slide * 100) * -1;
+			$('.gallery-block_slider_line').css('transform', 'translateX('+mg_line+'%)');
+			
+			$('.gallery-block_head_slider_number_item.number_page').text(num_slide + 1);
 		}
 	});
 });
